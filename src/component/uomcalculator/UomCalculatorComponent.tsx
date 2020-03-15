1619a98d9306070216b8a77bf8316d2d3c29c110
Michael Vlasaty <mvlasaty23@gmail.com>
2020-03-15 23:22:23 +0100

    working on uom calc

--- src/component/uomcalculator/UomCalculatorComponent.tsx
+++ src/component/uomcalculator/UomCalculatorComponent.tsx
@@ -1,5 +1,18 @@
-import { FormControl, Grid, IconButton, MenuItem, Select, TextField } from '@material-ui/core';
-import { Autorenew } from '@material-ui/icons';
+import {
+  FormControl,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  MenuItem,
+  Select,
+  TextField,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import { Add, Autorenew, Delete } from '@material-ui/icons';
 import React, { useState } from 'react';
 
 interface UnitOfMeasure {
@@ -43,39 +56,84 @@ const uoms: UnitOfMeasure[] = [
 function findUom(id: string) {
   return uoms.find(uom => uom.id === id) || uoms[1];
 }
+
+interface Amount {
+  uom: string;
+  amount: number;
+}
+interface Item {
+  name: string;
+  to: Amount;
+}
+
+const useStyles = makeStyles(() => ({
+  fullWidth: {
+    width: '100%',
+  },
+  center: {
+    textAlign: 'center',
+  },
+}));
+
 export default function UomCalculatorComponent() {
+  const classes = useStyles();
+
   const [fromUom, setFromUom] = useState(uoms[2]);
   const handleFromUomChange = (event: React.ChangeEvent<any>) => setFromUom(findUom(event.target.value));
   const [toUom, setToUom] = useState(uoms[0]);
   const handleToUomChange = (event: React.ChangeEvent<any>) => setToUom(findUom(event.target.value));
-  const [from, setFrom] = useState('');
+  const [from, setFrom] = useState(0);
   const handleFromChange = (event: React.ChangeEvent<any>) => {
     setFrom(event.target.value);
   };
-
-  let to = '';
-  if (fromUom.baseFactor > toUom.baseFactor) {
-    to = ((parseInt(from, 10) * fromUom.baseFactor) / toUom.baseFactor).toString();
-  } else if (fromUom.baseFactor < toUom.baseFactor) {
-    to = (parseInt(from, 10) / fromUom.baseFactor / toUom.baseFactor).toString();
-  } else {
-    to = from;
-  }
-
   const switchUom = () => {
     const tmpFrom = fromUom;
     setFromUom(toUom);
     setToUom(tmpFrom);
   };
 
+  let to = 0;
+  if (fromUom.baseFactor > toUom.baseFactor) {
+    to = (from * fromUom.baseFactor) / toUom.baseFactor;
+  } else if (fromUom.baseFactor < toUom.baseFactor) {
+    to = from / fromUom.baseFactor / toUom.baseFactor;
+  } else {
+    to = from;
+  }
+
+  const [itemName, setItemName] = useState('');
+  const handleItemNameChange = (event: React.ChangeEvent<any>) => setItemName(event.target.value);
+  const [items, setItems] = useState<Item[]>([]);
+  const handleAddItem = () =>
+    setItems([
+      ...items,
+      {
+        name: itemName,
+        to: {
+          uom: toUom.short,
+          amount: to,
+        },
+      },
+    ]);
+  const handleRemoveItem = (item: Item) => () => setItems(items.filter(i => i.name !== item.name));
+
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
-        <h1>Unit of Measure Calculator</h1>
+        <Typography variant="h5">Maßeinheiten Rechner</Typography>
       </Grid>
-      <Grid item xs={12}>
-        <TextField id="input-from-amount" placeholder="From" value={from} onChange={handleFromChange} />
-        <FormControl>
+      <Grid item xs={9}>
+        <TextField
+          id="input-from-amount"
+          placeholder="From"
+          value={from}
+          onChange={handleFromChange}
+          type="number"
+          className={classes.fullWidth}
+        />
+      </Grid>
+      <Grid item xs={3}>
+        <FormControl className={classes.fullWidth}>
           <Select id="select-from-uom" value={fromUom.id} onChange={handleFromUomChange}>
             {uoms.map(uom => (
               <MenuItem key={uom.id} value={uom.id}>
@@ -85,14 +143,23 @@ export default function UomCalculatorComponent() {
           </Select>
         </FormControl>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} className={classes.center}>
         <IconButton aria-label={'Switch'} onClick={switchUom}>
           <Autorenew />
         </IconButton>
       </Grid>
-      <Grid item xs={12}>
-        <TextField id="input-to-amount" placeholder="To" value={to} />
-        <FormControl>
+      <Grid item xs={9}>
+        <TextField
+          id="input-to-amount"
+          placeholder="To"
+          value={to}
+          type="number"
+          disabled
+          className={classes.fullWidth}
+        />
+      </Grid>
+      <Grid item xs={3}>
+        <FormControl className={classes.fullWidth}>
           <Select id="select-to-uom" value={toUom.id} onChange={handleToUomChange}>
             {uoms
               .filter(uom => uom.category === fromUom.category)
@@ -103,6 +170,43 @@ export default function UomCalculatorComponent() {
               ))}
           </Select>
         </FormControl>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h5">Zutaten Liste</Typography>
+      </Grid>
+      <Grid item xs={10}>
+        <TextField
+          id="input-add-item"
+          placeholder="Zutat hinzufügen"
+          value={itemName}
+          onChange={handleItemNameChange}
+          className={classes.fullWidth}
+        />
+      </Grid>
+      <Grid item xs={2}>
+        <IconButton
+          color="primary"
+          aria-label="add item"
+          component="span"
+          onClick={handleAddItem}
+          disabled={itemName === ''}
+        >
+          <Add />
+        </IconButton>
+      </Grid>
+      <Grid item xs={12}>
+        <List>
+          {items.map(item => (
+            <ListItem key={item.name}>
+              <ListItemText primary={`${item.to.amount.toFixed(2)} ${item.to.uom}`} secondary={item.name} />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete" onClick={handleRemoveItem(item)}>
+                  <Delete />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
       </Grid>
     </Grid>
   );
